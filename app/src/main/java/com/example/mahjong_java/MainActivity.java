6e2d673370761e7f35f152e21746ae02cbd6b7da
doinaoki <119909153+doinaoki@users.noreply.github.com>
2024-04-21 05:01:14 +0900

    feat: page transition from MainActivity to SelectQuestion

--- app/src/main/java/com/example/mahjong_java/MainActivity.java
+++ app/src/main/java/com/example/mahjong_java/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mahjong_java;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SelectQuestionActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
