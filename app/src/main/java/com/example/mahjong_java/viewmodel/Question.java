e45dd225f470c2b07d44ca6860d59e1eb9c29f16
doinaoki <119909153+doinaoki@users.noreply.github.com>
2024-05-05 02:03:26 +0900

    feat: Question class

--- app/src/main/java/com/example/mahjong_java/viewmodel/Question.java
+++ app/src/main/java/com/example/mahjong_java/viewmodel/Question.java
@@ -1,7 +1,35 @@
 package com.example.mahjong_java.viewmodel;
 
-public class Question {
-    public Question(){
+import java.util.HashMap;
+import java.util.List;
 
+public class Question {
+    private Hand hand;
+    private List<Integer> winPieces;
+    private HashMap<Integer, List<String>> roles = null;
+
+    public Hand getHand() {
+        return hand;
     }
+
+    public List<Integer> getWinPieces() {
+        return winPieces;
+    }
+
+    public HashMap<Integer, List<String>> getRoles(){
+        return roles;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public void setWinPieces(List<Integer> winPieces) {
+        this.winPieces = winPieces;
+    }
+
+    public void setRoles(HashMap<Integer, List<String>> roles) {
+        this.roles = roles;
+    }
+
 }
